768fbe534ef614557a10480bc232122538b5db31
AhmadGitacc <danganaahmed5@gmail.com>
2026-01-28 02:04:01 +0000

    edit auth middleware to check bearer for token

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -4,7 +4,11 @@ import { getUserBySessionToken } from "../models/User";
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const sessionToken = req.cookies["REHAB-AUTH"]
+        let sessionToken = req.cookies["REHAB-AUTH"]
+
+        if (!sessionToken && req.headers.authorization) {
+            sessionToken = req.headers.authorization.split(' ')[1];
+        }
 
         if (!sessionToken) {
             return res.sendStatus(403)
@@ -30,8 +34,8 @@ export const isOwner = async (req: express.Request, res: express.Response, next:
     try {
         const { id } = req.params
 
-        const currentUserId = get(req,'identity._id') as string;
-        const currentUserRole = get(req,'identity.role') as string;
+        const currentUserId = get(req, 'identity._id') as string;
+        const currentUserRole = get(req, 'identity.role') as string;
 
         if (!currentUserId) {
             return res.sendStatus(403)
@@ -55,8 +59,8 @@ export const isOwner = async (req: express.Request, res: express.Response, next:
 }
 
 export const isAdmin = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    try{
-        const currentUser = get(req,'identity.role') as string;
+    try {
+        const currentUser = get(req, 'identity.role') as string;
 
         if (currentUser !== 'admin' && currentUser !== 'doctor') {
             return res.status(403).json({ message: 'Admin access required' });
@@ -64,7 +68,7 @@ export const isAdmin = async (req: express.Request, res: express.Response, next:
 
         return next();
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
         return res.sendStatus(400)
     }
